2946897ef62e8e9805122eea0d4b7ad1accfa7c3
chun <1414975153@qq.com>
2019-02-17 19:55:41 +0800

    commit

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -2,8 +2,10 @@
 # @Time    : 2019/1/28 9:30
 # @Author  : llc
 # @File    : __init__.py.py
+
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
-from .widgets import TitleBar, TabMenuBar, GroupWidget
+from .widgets import TitleBar, MenuBar, GroupWidget
 
 
 class _QRibbonWidget(QWidget):
@@ -11,32 +13,28 @@ class _QRibbonWidget(QWidget):
         super(_QRibbonWidget, self).__init__()
         self._ribbonwindow = ribbonwindow
 
-        self.title_bar = TitleBar(self)
+        self.title_bar = TitleBar(self._ribbonwindow)
         self.title_bar.button_close.clicked.connect(self._ribbonwindow.close)
         self.title_bar.button_max.clicked.connect(self._ribbonwindow_showMaximized)
         self.title_bar.button_min.clicked.connect(self._ribbonwindow.showMinimized)
         self._ribbonwindow.double_click.connect(self._ribbonwindow_showMaximized)
 
-        self.tabmenu_bar = TabMenuBar(self)
-        #
-        # self.group_widget = GroupWidget(self)
+        self.menu_bar = MenuBar(self._ribbonwindow)
 
         self._init_ui()
+        self.setMouseTracking(True)
 
     def _init_ui(self):
         _vl = QVBoxLayout(self)
         _vl.setContentsMargins(0, 0, 0, 0)
         _vl.setSpacing(0)
         _vl.addWidget(self.title_bar)
-
-        _vl.addWidget(self.tabmenu_bar)
-
-        # _vl.addWidget(self.group_widget)
+        _vl.addWidget(self.menu_bar)
 
     def _ribbonwindow_showMaximized(self):
         if not self._ribbonwindow.isMaximized():
             self._ribbonwindow.showMaximized()
-            self.title_bar.button_max.setText('2')
+            self.title_bar.button_max.setIcon(QIcon(':icons/images/max2.png'))
         else:
             self._ribbonwindow.showNormal()
-            self.title_bar.button_max.setText('1')
+            self.title_bar.button_max.setIcon(QIcon(':icons/images/max1.png'))
